5ad7e1c75438cbfbe2ac45b3ef2974bae9cda5d9
Kauan Vieira <kauan.ssv0@gmail.com>
2024-11-21 19:25:52 -0300

    aula 3 - 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
-      <h2>Olá mundo!!!</h2>
-      <p>Meu primeiro projeto usando React.js</p>
+      <HelloWorld/>
+      <SayMyName nome='Matheus'/>   
+      <Pessoa nome='Kauan' idade='20' profissao='Programador Front-End' foto='https://via.placeholder.com/150'/>   
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import Frase from "./Frase"
+
+function HelloWorld(){
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
